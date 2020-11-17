051c877c06ecc27b65b91c9c5ff89afe6ecd394c
bryan.helsens <bryan.helsens@student.howest.be>
2020-11-17 12:47:24 +0100

    identity

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,3 +36,11 @@ exports.createPages = async ({actions, graphql, reporter}) => {
         })
     })
 };
+
+exports.onCreatePage = async ({ page, actions }) => {
+    const { createPage } = actions;
+    if (page.path.match(/^\/account/)){
+        page.matchPath = "/account/*";
+        createPage(page);
+    }
+};
